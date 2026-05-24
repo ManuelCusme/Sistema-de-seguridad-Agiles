4e25984895c71f9111816a0ed6a89958984fe1cd
Manuel Cusme <cusmeman06@gmail.com>
2026-05-24 13:06:59 -0500

    feat(integration): route zones and seed geofences

--- Database.sql
+++ Database.sql
@@ -45,12 +45,26 @@ IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[Ge
 BEGIN
     CREATE TABLE Geofences (
         Id        UNIQUEIDENTIFIER  PRIMARY KEY DEFAULT NEWID(),
+        Code      NVARCHAR(10)      NOT NULL,
         Name      NVARCHAR(100)     NOT NULL,
-        Latitude  FLOAT             NOT NULL,
-        Longitude FLOAT             NOT NULL,
-        Radius    FLOAT             NOT NULL  -- en metros
+            Boundary  GEOGRAPHY         NOT NULL,
+            Latitude  FLOAT             NULL,
+            Longitude FLOAT             NULL,
+            Radius    FLOAT             NULL
     );
     PRINT 'Tabla Geofences creada correctamente.';
+END
+GO
+
+IF COL_LENGTH('dbo.Geofences', 'Code') IS NULL
+BEGIN
+    ALTER TABLE Geofences ADD Code NVARCHAR(10) NULL;
+END
+GO
+
+IF COL_LENGTH('dbo.Geofences', 'Boundary') IS NULL
+BEGIN
+    ALTER TABLE Geofences ADD Boundary GEOGRAPHY NULL;
 END
 GO
 
@@ -117,14 +131,14 @@ GO
 -- ============================================================
 -- SEED: Zonas del campus Huachi (4 zonas operativas)
 -- ============================================================
-IF NOT EXISTS (SELECT * FROM Geofences WHERE Name = 'Zona1')
+IF NOT EXISTS (SELECT * FROM Geofences WHERE Boundary IS NOT NULL)
 BEGIN
-    INSERT INTO Geofences (Id, Name, Latitude, Longitude, Radius)
+    INSERT INTO Geofences (Id, Code, Name, Boundary, Latitude, Longitude, Radius)
     VALUES
-        (NEWID(), 'Zona1', -1.267490, -78.624756, 200),
-        (NEWID(), 'Zona2', -1.267525, -78.623603, 200),
-        (NEWID(), 'Zona3', -1.269470, -78.625026, 200),
-        (NEWID(), 'Zona4', -1.269750, -78.623427, 200);
+        (NEWID(), 'Z1', 'FACULTAD DE INGENIERÍA', geography::STPolyFromText('POLYGON ((-78.625301 -1.266416, -78.624212 -1.26648, -78.624212 -1.268564, -78.62584 -1.268564, -78.625301 -1.266416))', 4326), -1.267490, -78.624756, 200),
+        (NEWID(), 'Z2', 'BIBLIOTECA GENERAL', geography::STPolyFromText('POLYGON ((-78.624212 -1.26648, -78.622994 -1.266555, -78.62264 -1.268564, -78.624212 -1.268564, -78.624212 -1.26648))', 4326), -1.267525, -78.623603, 200),
+        (NEWID(), 'Z3', 'RECTORADO / ADMINISTRACIÓN', geography::STPolyFromText('POLYGON ((-78.62584 -1.268564, -78.624212 -1.268564, -78.624212 -1.27065, -78.62638 -1.270376, -78.62584 -1.268564))', 4326), -1.269470, -78.625026, 200),
+        (NEWID(), 'Z4', 'COMPLEJO DEPORTIVO', geography::STPolyFromText('POLYGON ((-78.624212 -1.268564, -78.62264 -1.268564, -78.622289 -1.270935, -78.624212 -1.27065, -78.624212 -1.268564))', 4326), -1.269750, -78.623427, 200);
     PRINT '4 zonas del campus Huachi insertadas correctamente.';
 END
 GO
